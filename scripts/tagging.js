a333555240142bbd6dfafcbbce696c6e4b479d65
David <danstee1@my.bcit.ca>
2024-11-07 11:41:04 -0800

    Change how tags are added

--- scripts/tagging.js
+++ scripts/tagging.js
@@ -1,23 +1,45 @@
-const tags = ["quiz", "lab"];
+const tags = ["Quiz", "Lab"];
 const cards = [];
 var $contextMenu = $("#contextMenu");
 
 function openTags(event) {
     let tagList = "";
-    tags.forEach(function(item) {
+    tags.forEach(function (item) {
         tagList += `<button class="dropdown-item" type="button">` + item + `</button>`;
     });
-    tagList += `<div class="dropdown-divider"></div> <button class="dropdown-item" type="button">Create new</button>`
+    tagList += `<div class="dropdown-divider"></div> <button onClick="prompt_new_tag()" class="dropdown-item" type="button">Create new</button>`
     $("#contextMenu")
         .empty()
         .append(tagList)
-        .css({position: "absolute",
+        .css({
+            position: "absolute",
             left: event.pageX,
             top: event.pageY
         })
         .show()
         .trigger("focusin");
     return;
+}
+
+function validate_tag(tag) {
+    if (tag == "") return 1;
+    if (tag == null) return 2;
+    for (let i = 0; i < tags.length; ++i) {
+        if (tag.toLowerCase() == tags[i].toLowerCase()) return 3;
+    }
+    return 0;
+}
+
+function prompt_new_tag() {
+    console.log("called");
+    let user_tag = window.prompt("Enter new tag:", "");
+    let valid = validate_tag(user_tag);
+    if (valid == 0) {
+        tags.push(user_tag);
+        console.log("new tag:" + user_tag);
+    }
+    else if (valid == 1) { window.alert("No tag entered!"); }
+    else if (valid == 3) { window.alert("Tag already exists!") }
 }
 
 function new_tag() {
@@ -29,14 +51,13 @@ function new_tag() {
     }
 }
 function setup() {
-    $("#add_tag").click(new_tag)
-    $(window).on("click", function(e) {
+    $(window).on("click", function (e) {
         $("#contextMenu").hide();
     })
-    $(".tag-list").on("click", function(e) {
+    $(".tag-list").on("click", function (e) {
         openTags(e);
         e.stopPropagation();
-    })    
+    })
     console.log('setup');
 }
 $(document).ready(setup);
