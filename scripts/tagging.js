4dc5bb633e5f435beef40748b01f99f6d996d66e
David <danstee1@my.bcit.ca>
2024-11-07 19:19:43 -0800

    Revise tagging system

--- scripts/tagging.js
+++ scripts/tagging.js
@@ -1,6 +1,52 @@
 const tags = ["Quiz", "Lab"];
-const cards = [];
-var $contextMenu = $("#contextMenu");
+
+const cardDataTemplate = {
+    title: "template",
+    course: "debug",
+    assignDate: "1970-01-01",
+    dueDate: "1970-01-01",
+    cardTags: [],
+    customOrder: -1
+}
+
+function createCardData(_title, _course, _assignDate, _dueDate, _tags) {
+    let newCard = { ...cardDataTemplate };
+    newCard.title = _title;
+    newCard.course = _course;
+    newCard.assignDate = _assignDate;
+    newCard.dueDate = _dueDate;
+    newCard.cardTags = _tags;
+    return newCard;
+}
+
+function createCardFromData(data) {
+    let newCard_html = $("#card-template").html();
+    const container = $("#card-container");
+    const newCard_elem = $(newCard_html);
+
+    newCard_elem.data("cardData", data);
+    newCard_elem.find(".card-assignment-title").text(data.title);
+    newCard_elem.find(".card-course-title").text(data.course);
+    newCard_elem.find(".card-assign-date").text(data.assignDate);
+    newCard_elem.find(".card-due-date").text(data.dueDate);
+    newCard_elem.find(".card-id").text(data.id);
+    tags.forEach(function (tag) {
+        let tag_entry = `<li><button class="tag-item dropdown-item `;
+        if (data.cardTags.includes(tag)) { tag_entry += `fw-semibold bg-greyed-out`; console.log("cringe") };
+        tag_entry += `">` + tag + `</button></li>`;
+        newCard_elem.find(".dynamic-tags").prepend(tag_entry)
+    })
+    console.log(newCard_elem);
+    container.append(newCard_elem);
+}
+
+function createExamples() {
+    createCardFromData(createCardData("Assignment 1", "COMP-1800", "2024-09-23", "2024-09-30", ['Assignment']));
+    createCardFromData(createCardData("Lab 2", "COMP-1537", "2024-10-09", "2024-10-16", ['Lab']));
+    createCardFromData(createCardData("Quiz 3", "COMP-1510", "2024-11-30", "2024-11-30", ['Quiz']));
+    createCardFromData(createCardData("Term Project", "COMP-1712", "2024-10-20", "2024-12-02", ['Project']));
+    createCardFromData(createCardData("Demo 1", "COMP-1800", "2024-09-20", "2024-10-01", ['Demo']));
+}
 
 function openTags(event) {
     let tagList = "";
@@ -30,33 +76,49 @@ function validate_tag(tag) {
     return 0;
 }
 
+function update_tags() {
+    $(".dynamic-tags").each(function () {
+        const tag_list = $(this);
+        tag_list.empty();
+        tags.forEach(function (tag) {
+            let tag_entry = `<li><button class="tag-item dropdown-item `;
+            if (tag_list.closest(".card").data("cardData").cardTags.includes(tag)) tag_entry += `fw-semibold bg-greyed-out`;
+            tag_entry += `">` + tag + `</button></li>`;
+            tag_list.prepend(tag_entry);
+        })
+    });
+    console.log("updated");
+}
+
 function prompt_new_tag() {
-    console.log("called");
+    //console.log("called");
     let user_tag = window.prompt("Enter new tag:", "");
     let valid = validate_tag(user_tag);
     if (valid == 0) {
         tags.push(user_tag);
-        console.log("new tag:" + user_tag);
+        update_tags();
     }
     else if (valid == 1) { window.alert("No tag entered!"); }
     else if (valid == 3) { window.alert("Tag already exists!") }
 }
 
-function new_tag() {
-    user_tag = $("#tag_input").val().toLowerCase();
-    if (user_tag != "" && !tags.includes(user_tag)) {
-        tags.push(user_tag);
-        console.log("new tag:" + user_tag);
-        $("#tag_input").val("");
-    }
-}
 function setup() {
-    $(window).on("click", function (e) {
-        $("#contextMenu").hide();
+    createExamples();
+
+    $("body").on("click", ".new-tag", function (e) {
+        prompt_new_tag();
     })
-    $(".tag-list").on("click", function (e) {
-        openTags(e);
-        e.stopPropagation();
+    $("body").on("click", ".tag-item", function () {
+        let info = { tag: $(this).text(), object: $(this).closest(".card") };
+        if (info.object.data("cardData").cardTags.includes(info.tag)) {
+            let i = info.object.data("cardData").cardTags.indexOf(info.tag);
+            info.object.data("cardData").cardTags.splice(i);
+        }
+        else {
+            info.object.data("cardData").cardTags.push(info.tag);
+        }
+        update_tags();
+        console.log(info.object.data("cardData").cardTags)
     })
     console.log('setup');
 }
