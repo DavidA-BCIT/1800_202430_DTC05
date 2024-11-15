7dab0072c534ec2c6a7a319cbacc0ad79df398ec
Fereshteh Aghaarabi <faghaarabi@my.bcit.ca>
2024-11-15 00:11:08 -0800

    !

--- scripts/notification copy.js
+++ scripts/notification copy.js
@@ -1,10 +1,7 @@
-
-// Function to read the quote of the day from the Firestore "quotes" collection
-// Input param is the String representing the day of the week, aka, the document name
 function readQuote(day) {
     db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
         .onSnapshot(dayDoc => {                                                              //arrow notation
-            console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+            console.log("current document data: "+ dayDoc.data());                          //.data() returns data object
             document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quotes;      //using javascript to display the data on the right place
 
             //Here are other ways to access key-value data fields
@@ -16,4 +13,4 @@ function readQuote(day) {
             console.log ("Error calling onSnapshot", error);
         });
     }
- readQuote("Friday");        //calling the function
+    readQuote("tuesday");  
