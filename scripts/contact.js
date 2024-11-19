f56fb1bcc104b5b2ab9cbe27cd456144784b6cbd
David <danstee1@my.bcit.ca>
2024-11-18 18:12:49 -0800

    Prototype clipboard button

--- scripts/contact.js
+++ scripts/contact.js
@@ -52,6 +52,13 @@ function setContactInfo() {
     })
 }
 
+async function setClipboard(text) {
+    const type = "text/plain";
+    const blob = new Blob([text], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+    await navigator.clipboard.write(data);
+}
+
 function setup() {
     authenticateUser();
     $("#btn-edit").on("click", function () {
@@ -60,6 +67,9 @@ function setup() {
     $("#btn-save").on("click", function () {
         $("#contactInfoFields").prop("disabled", true)
         setContactInfo();
+    })
+    $(".btn-clipboard").on("click", function () {
+        setClipboard("test");
     })
 }
 
